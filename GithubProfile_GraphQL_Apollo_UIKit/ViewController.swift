d6b160b548d90c2fbe51800ba74dc83dbea6c3c4
wjdgkals23 <wjdgkals74@gmail.com>
2022-02-06 09:53:43 +0900

    inital Setting

---
+++ GithubProfile_GraphQL_Apollo_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubProfile_GraphQL_Apollo_UIKit
+//
+//  Created by 정하민 on 2022/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
